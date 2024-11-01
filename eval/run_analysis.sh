6259897e06139f52bf7de0e8619631b7135e9570
Dhruvit Patel <dpatel505@gmail.com>
2024-11-01 16:00:37 -0500

    Added normalization to dataloader.

---
+++ eval/run_analysis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --time=0-15:00:00
+#SBATCH -N 1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=60G
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:a10:1
+#SBATCH -o run_analysis.out
+#SBATCH -e run_analysis.err
+
+#SBATCH --account=145439188689
+
+cd /home/u.dp200518/SSL-Wavelets/eval
+
+source activate /scratch/user/u.dp200518/.conda/envs/2DTurbEmulator
+
+python analysis.py
